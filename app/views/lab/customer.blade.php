bcad1bfce63a20a409409cf08d13525784c46acb
develop <andres_gomez@ieee.org>
2014-08-25 23:19:21 -0500

    customer lab buttons added

--- app/views/lab/customer.blade.php
+++ app/views/lab/customer.blade.php
@@ -3,9 +3,13 @@
 
 @section("content")
 	@if(isset($customer))
-	<a href="{{ URL::current() }}/nuevaOrden" class="btn btn-success btn-lg active pull-right" role="button">Nuevo Orden</a>
 		<h1>{{ $customer[0]->name}}</h1>
 		<div id='isotopes' class='clickable isotope clearfix'>
+			<div class="center-block row btn-group" >
+			<a href="{{ URL::current() }}/nuevaOrden" class="btn btn-primary btn-lg active col-lg-3" role="button">Pagos</a>
+			<a href="{{ URL::action('LabController@addOrderTo', $customer[0]->id ) }}" class="btn btn-success btn-lg active col-lg-3" role="button">Nueva Orden</a>
+			<a href="{{ URL::current() }}/nuevaOrden" class="btn btn-warning btn-lg active col-lg-3" role="button">Canceladas</a>
+		</div>
 		@if(isset($orders_pending))
 			@foreach ($orders_peding  as $order) 
 				<div id='cliente' class='element cliente' data-symbol='Hg' data-category='lanthanoid'>
