a5c7bc33e4bf7a02a134cd3f58fc7cd6e196411b
develop <andres_gomez@ieee.org>
2014-08-27 18:59:29 -0500

    store user that create customer and new order

--- app/controllers/LabController.php
+++ app/controllers/LabController.php
@@ -24,20 +24,24 @@ class LabController extends BaseController {
 	public function create()
 	{
 		$new = new LabCustomer;
-		$new->name = Input::get("name");
-		$new->address = Input::get("address");
-		$new->telephone = Input::get("telephone");
-		$new->email = Input::get("email");
-		$new->discountStd = Input::get("discountStd");
-		$new->discountSpc = Input::get("discountSpc");
-		$new->save();
+		$new-> name = Input::get("name");
+		$new-> address = Input::get("address");
+		$new-> telephone = Input::get("telephone");
+		$new-> email = Input::get("email");
+		$new-> discountStd = Input::get("discountStd");
+		$new-> discountSpc = Input::get("discountSpc");
+		$new-> updated_by = Auth::user()-> username;
+		$new-> save();
 		return $this-> index();
 	}
 
 	public function addOrderTo($id)
 	{
 		$newOrder = new LabOrder();
-		$newOrder->status =  'open';
+		$newOrder-> status =  'open';
+		$newOrder-> created_by = Auth::user()-> username;
+		$newOrder-> delivery_date = new DateTime('today');
+		$newOrder-> tax = '16.0';
 		$customer = LabCustomer::find($id)-> orders()-> save($newOrder);
 		return $this-> show($id);
 	}
@@ -75,7 +79,7 @@ class LabController extends BaseController {
 	 */
 	public function show($id)
 	{
-		$orders_peding = LabCustomer::find($id)->orders()->get();
+		$orders_peding = LabCustomer::find($id)-> orders()-> get();
 		$customer = LabCustomer::find($id);
 		return View::make("lab.customer", array( "orders_peding" => $orders_peding,
 													"customer"	=> $customer) );
