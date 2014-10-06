bc54f90b125e2f08804cd8342667a09d9b8960f2
develop <andres_gomez@ieee.org>
2014-10-05 19:21:34 -0500

    added personal id to pacient table

--- app/database/migrations/2014_10_03_004931_CreatePacientTable.php
+++ app/database/migrations/2014_10_03_004931_CreatePacientTable.php
@@ -5,57 +5,61 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreatePacientTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('pacient', function(Blueprint $table)
-		{
-			$table->increments("id");
-			$table
-						->string("first_name")
-						->default("N/A") ;
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('pacient', function(Blueprint $table)
+    {
+      $table->increments("id");
+      $table
+            ->string("personal_id")
+            ->default("");
 
-			$table
-						->string("last_name")
-						->default("N/A");
-			
-			$table
-						->string("address_home")
-						->default("");
-			
-			$table
-						->string("address_work")
-						->default("");
-						
-			$table
-						->string("email")
-						->default("");
+      $table
+            ->string("first_name")
+            ->default("N/A") ;
 
-			$table
-						->string("telephone")
-						->default("");
+      $table
+            ->string("last_name")
+            ->default("N/A");
+      
+      $table
+            ->string("address_home")
+            ->default("");
+      
+      $table
+            ->string("address_work")
+            ->default("");
+            
+      $table
+            ->string("email")
+            ->default("");
 
-			$table
-						->date("dob")
-						->nullable()
-						->default(null);
-			
-			$table-> timestamps();
-		});
-	}
+      $table
+            ->string("telephone")
+            ->default("");
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('pacient');
-	}
+      $table
+            ->date("dob")
+            ->nullable()
+            ->default(null);
+      
+      $table-> timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('pacient');
+  }
 
 }
