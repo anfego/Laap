8a192fdbb9117753683e7dfc88091e65ae7737ef
develop <andres_gomez@ieee.org>
2014-11-15 17:23:30 -0600

    reduce data entry

--- app/views/optometrist/examination/new.blade.php
+++ app/views/optometrist/examination/new.blade.php
@@ -10,11 +10,11 @@
     <li role="presentation"><a href="#vp" aria-controls="vp" role="tab" data-toggle="tab">VP</a></li>
     <li role="presentation"><a href="#contactLenses" aria-controls="contactLenses" role="tab" data-toggle="tab">Lentes de Contacto</a></li>
     <li role="presentation"><a href="#keratometer" aria-controls="keratometer" role="tab" data-toggle="tab">Queratometria</a></li>
-    <li role="presentation"><a href="#lensmetric" aria-controls="lensmetric" role="tab" data-toggle="tab">Lensometria</a></li>
+    <li role="presentation"><a href="#lensmetric" aria-controls="lensmetric" role="tab" data-toggle="tab">Lensométria</a></li>
     <li role="presentation"><a href="#cicloRx" aria-controls="cicloRx" role="tab" data-toggle="tab">Rx Ciclopejica</a></li>
     <li role="presentation"><a href="#subjective" aria-controls="subjective" role="tab" data-toggle="tab">Subjetivo</a></li>
     <li role="presentation"><a href="#rx" aria-controls="rx" role="tab" data-toggle="tab">Rx Final</a></li>
-    <li role="presentation"><a href="#subjective" aria-controls="subjective" role="tab" data-toggle="tab">Subjetivo</a></li>
+    <li role="presentation"><a href="#subjective" aria-controls="subjective" role="tab" data-toggle="tab">Oftal. Directa</a></li>
   </ul>
 
   <!-- Tab panes -->
@@ -28,14 +28,14 @@
       {{ $errors->first("password") }}<br />
       {{ Form::field([
             "name"        => "ocupation",
-            "label"       => "Ocupacion",
-            "placeholder" => "Ocupacion",
+            "label"       => "Ocupación",
+            "placeholder" => "Ocupación",
             "type"        => "text"
       ]) }}
       {{ Form::field([
           "name"        => "motivation",
-          "label"       => "Motivacion",
-          "placeholder" => "Motivacion",
+          "label"       => "Motivo de Consulta",
+          "placeholder" => "Escriba acá",
           "type"        => "textArea"
       ]) }}
     </div>
@@ -48,7 +48,7 @@
       ]) }}
       {{ Form::field([
           "name"        => "vl_left",
-          "label"       => "vl_right",
+          "label"       => "vl_left",
           "placeholder" => "0.00",
           "type"        => "text"
       ]) }}
@@ -117,20 +117,8 @@
           "type"        => "text"
       ]) }}
       {{ Form::field([
-          "name"        => "ktX_right",
-          "label"       => "ktX_right",
-          "placeholder" => "0.00",
-          "type"        => "text"
-      ]) }}
-      {{ Form::field([
           "name"        => "kt_left",
           "label"       => "kt_left",
-          "placeholder" => "0.00",
-          "type"        => "text"
-      ]) }}
-      {{ Form::field([
-          "name"        => "ktX_left",
-          "label"       => "ktX_left",
           "placeholder" => "0.00",
           "type"        => "text"
       ]) }}
@@ -143,20 +131,8 @@
           "type"        => "text"
       ]) }}
       {{ Form::field([
-          "name"        => "lxX_right",
-          "label"       => "lxX_right",
-          "placeholder" => "0.00",
-          "type"        => "text"
-      ]) }}
-      {{ Form::field([
           "name"        => "lx_left",
           "label"       => "lx_left",
-          "placeholder" => "0.00",
-          "type"        => "text"
-      ]) }}
-      {{ Form::field([
-          "name"        => "lxX_left",
-          "label"       => "lxX_left",
           "placeholder" => "0.00",
           "type"        => "text"
       ]) }}
@@ -170,20 +146,8 @@
           "type"        => "text"
       ]) }}
       {{ Form::field([
-          "name"        => "crX_right",
-          "label"       => "crX_right",
-          "placeholder" => "0.00",
-          "type"        => "text"
-      ]) }}
-      {{ Form::field([
           "name"        => "cr_left",
           "label"       => "cr_left",
-          "placeholder" => "0.00",
-          "type"        => "text"
-      ]) }}
-      {{ Form::field([
-          "name"        => "crX_left",
-          "label"       => "crX_left",
           "placeholder" => "0.00",
           "type"        => "text"
       ]) }}
@@ -196,20 +160,8 @@
           "type"        => "text"
       ]) }}
       {{ Form::field([
-          "name"        => "subX_right",
-          "label"       => "subX_right",
-          "placeholder" => "0.00",
-          "type"        => "text"
-      ]) }}
-      {{ Form::field([
           "name"        => "sub_left",
           "label"       => "sub_left",
-          "placeholder" => "0.00",
-          "type"        => "text"
-      ]) }}
-      {{ Form::field([
-          "name"        => "subX_left",
-          "label"       => "subX_left",
           "placeholder" => "0.00",
           "type"        => "text"
       ]) }}
@@ -222,20 +174,8 @@
           "type"        => "text"
       ]) }}
       {{ Form::field([
-          "name"        => "rxX_right",
-          "label"       => "rxX_right",
-          "placeholder" => "0.00",
-          "type"        => "text"
-      ]) }}
-      {{ Form::field([
           "name"        => "rx_left",
           "label"       => "rx_left",
-          "placeholder" => "0.00",
-          "type"        => "text"
-      ]) }}
-      {{ Form::field([
-          "name"        => "rxX_left",
-          "label"       => "rxX_left",
           "placeholder" => "0.00",
           "type"        => "text"
       ]) }}
